e7b639b344bd00139ad9ca0682ce7ad8bd959a55
Colin Alston <colin.alston@gmail.com>
2016-01-15 21:06:03 +0200

    Fix packages

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     description="A distributed service for managing container databases and shared storage",
     author='Colin Alston',
     author_email='colin@praekelt.com',
+    packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'Twisted',
