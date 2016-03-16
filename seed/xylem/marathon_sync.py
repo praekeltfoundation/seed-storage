1dfcc6b70f136c0c22e45337688cf45bff6c9aee
Jeremy Thurgood <jeremy@praekelt.com>
2016-03-16 14:45:11 +0200

    First stab at a Marathon app sync plugin.

---
+++ seed/xylem/marathon_sync.py
@@ -0,0 +1,47 @@
+from twisted.internet.defer import gatherResults
+from twisted.web.client import getPage
+from twisted.web.http_headers import Headers
+
+from rhumba import RhumbaPlugin, cron
+
+
+class Plugin(RhumbaPlugin):
+    """
+    A plugin to periodically push an application group definition to Marathon.
+    """
+
+    def __init__(self, *args, **kw):
+        super(Plugin, self).__init__(*args, **kw)
+
+        self.marathon_host = self.config.get("marathon_host", "localhost")
+        self.marathon_port = self.config.get("marathon_port", "8080")
+        self.group_json_files = self.config["group_json_files"]
+
+    # @cron(min="*/1")
+    @cron(secs="*/10")
+    def call_do_thing(self, args):
+        self.log("Did thing: %r" % (args,))
+        return self.call_update_groups()
+
+    def call_update_groups(self):
+        ds = []
+        for group_json_file in self.group_json_files:
+            ds.append(self.call_update_group(group_json_file))
+        return gatherResults(ds)
+
+    def call_update_group(self, group_json_file):
+        self.log("Updating %r" % (group_json_file,))
+        with open(group_json_file) as f:
+            body = f.read()
+        d = self._call_marathon("PUT", "v2/groups", body)
+        d.addBoth(self._logcb)
+        return d
+
+    def _logcb(self, r, msg=" ... result: %r"):
+        self.log(msg % (r,))
+        return r
+
+    def _call_marathon(self, method, path, body=None):
+        uri = b"http://%s:%s/%s" % (
+            self.marathon_host, self.marathon_port, path)
+        return getPage(uri, method=method, postdata=body)
