3ef2e6841bff297cafc2d24307b0dd437581d733
Jeremy Thurgood <jeremy@praekelt.com>
2016-03-17 12:47:42 +0200

    Test more things.

--- seed/xylem/tests/test_marathon_sync.py
+++ seed/xylem/tests/test_marathon_sync.py
@@ -9,12 +9,9 @@ class TestMarathonSync(TestCase):
         kw.setdefault("name", "marathon_sync")
         return marathon_sync.Plugin(kw, None)
 
-    def fake_getPage(self, e_url, e_method, e_postdata, resp):
+    def fake_getPage(self, req_mapping):
         def getPage(url, method, postdata):
-            self.assertEqual(url, e_url)
-            self.assertEqual(method, e_method)
-            self.assertEqual(postdata, e_postdata)
-            return succeed(resp)
+            return succeed(req_mapping[(url, method, postdata)])
         return getPage
 
     def fake_readfile(self, fn_content_mapping):
@@ -31,9 +28,33 @@ class TestMarathonSync(TestCase):
         plugin.readfile = self.fake_readfile({
             "foo.json": '{"id": "/t", "apps": []}',
         })
-        plugin.getPage = self.fake_getPage(
-            "http://localhost:8080/v2/groups", "PUT",
-            '{"id": "/t", "apps": []}',
-            '{"version":"vvv","deploymentId":"ddd"}')
+        req = ("http://localhost:8080/v2/groups", "PUT",
+               '{"id": "/t", "apps": []}')
+        plugin.getPage = self.fake_getPage({
+            req: '{"version":"vvv","deploymentId":"ddd"}',
+        })
         resp = yield plugin.call_update_group({"group_json_file": "foo.json"})
         self.assertEqual(resp, '{"version":"vvv","deploymentId":"ddd"}')
+
+    @inlineCallbacks
+    def test_update_groups(self):
+        """
+        call_update_group makes an appropriate Marathon API request.
+        """
+        plugin = self.get_plugin(group_json_files=["foo.json", "bar.json"])
+        plugin.readfile = self.fake_readfile({
+            "foo.json": '{"id": "/f", "apps": []}',
+            "bar.json": '{"id": "/b", "apps": []}',
+        })
+        fooreq = ("http://localhost:8080/v2/groups", "PUT",
+                  '{"id": "/f", "apps": []}')
+        barreq = ("http://localhost:8080/v2/groups", "PUT",
+                  '{"id": "/b", "apps": []}')
+        plugin.getPage = self.fake_getPage({
+            fooreq: '{"version":"vvv","deploymentId":"dddf"}',
+            barreq: '{"version":"vvv","deploymentId":"dddb"}',
+        })
+        resp = yield plugin.call_update_groups({})
+        self.assertEqual(sorted(resp), sorted([
+            '{"version":"vvv","deploymentId":"dddf"}',
+            '{"version":"vvv","deploymentId":"dddb"}']))
