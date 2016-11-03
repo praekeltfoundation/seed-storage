fe9b8d38dc1ab8369758da00eb97e507e30fee4d
Jeremy Thurgood <jeremy@praekelt.org>
2016-11-03 13:06:46 +0200

    Test database creation API.

--- seed/xylem/tests/test_postgres.py
+++ seed/xylem/tests/test_postgres.py
@@ -6,6 +6,7 @@ except ImportError:
     pass
 import psycopg2
 from psycopg2 import errorcodes
+import pytest
 from twisted.internet.defer import inlineCallbacks
 from twisted.trial.unittest import TestCase
 
@@ -14,15 +15,28 @@ from seed.xylem import postgres
 
 def trap_pg_error(d, exc_type, pgcode=None):
     def trap_err(f):
-        f.trap(exc_type)
+        if not isinstance(f.value, exc_type):
+            return f
         if pgcode is not None and f.value.pgcode != pgcode:
             return f
     return d.addErrback(trap_err)
 
 
-def close_cursor(d, cur):
-    def close_cb(r):
+def close_cursor(cur):
+    if cur.running:
         cur.close()
+
+
+def passthrough(f, *args, **kw):
+    def cb(r):
+        f(*args, **kw)
+        return r
+    return cb
+
+
+def cleanup_cursor(d, cur):
+    def close_cb(r):
+        close_cursor(cur)
         return r
     return d.addBoth(close_cb)
 
@@ -52,25 +66,42 @@ class TestPostgresPlugin(TestCase):
         between tests.
         """
         plug = self.get_plugin_no_setup()
-        d = self.cleanup_database(plug)
+        d = self.cleanup_databases_table(plug)
         d.addCallback(lambda _: plug._setup_db())
         d.addCallback(lambda _: plug)
         return d
 
-    def cleanup_database(self, plug):
+    def cleanup_databases_table(self, plug):
         d = self.run_operation(plug, "DROP TABLE databases;")
         trap_pg_error(d, psycopg2.ProgrammingError, errorcodes.UNDEFINED_TABLE)
         return d
 
+    def dropdb(self, plug, dbname):
+        self.addCleanup(self._dropdb, plug, dbname)
+        return self._dropdb(plug, dbname)
+
+    def _dropdb(self, plug, dbname):
+        d = self.run_operation(plug, "DROP DATABASE %s;" % (dbname,))
+        trap_pg_error(
+            d, psycopg2.ProgrammingError, errorcodes.INVALID_CATALOG_NAME)
+        return d
+
     def run_query(self, plug, *args, **kw):
         cur = plug._get_xylem_db()
+        self.addCleanup(close_cursor, cur)
         d = cur.runQuery(*args, **kw)
-        return close_cursor(d, cur)
+        return d.addBoth(passthrough(close_cursor, cur))
 
     def run_operation(self, plug, *args, **kw):
         cur = plug._get_xylem_db()
+        self.addCleanup(close_cursor, cur)
         d = cur.runOperation(*args, **kw)
-        return close_cursor(d, cur)
+        return d.addBoth(passthrough(close_cursor, cur))
+
+    def list_dbs(self, plug):
+        d = self.run_query(
+            plug, "SELECT datname FROM pg_database WHERE NOT datistemplate;")
+        return d.addCallback(lambda r: [x[0] for x in r])
 
     @inlineCallbacks
     def assert_pg_error(self, d, exc_type, pgcode=None):
@@ -97,7 +128,7 @@ class TestPostgresPlugin(TestCase):
         We can create our own database table.
         """
         plug = self.get_plugin_no_setup()
-        yield self.cleanup_database(plug)
+        yield self.cleanup_databases_table(plug)
         # After cleanup, our table should not exist.
         yield self.assert_pg_error(
             self.run_query(plug, "SELECT * FROM databases"),
@@ -120,3 +151,120 @@ class TestPostgresPlugin(TestCase):
         # After a second setup, our table should still exist.
         rows = yield self.run_query(plug, "SELECT * FROM databases")
         self.assertEqual(rows, [])
+
+    @inlineCallbacks
+    def test_call_create_database_bad_name(self):
+        """
+        We can't create a database with a bad name.
+        """
+        plug = yield self.get_plugin()
+        names = ["", " ", ".", "-", '"; -- bobby tables']
+
+        for name in names:
+            r = yield plug.call_create_database({"name": name})
+            self.assertEqual(r, {"Err": "Database name must be alphanumeric"})
+
+    @inlineCallbacks
+    def test_call_create_database_new(self):
+        """
+        We can create a new database.
+        """
+        dbname = "xylem_test_create_new"
+        plug = yield self.get_plugin()
+        yield self.dropdb(plug, dbname)
+        dbs = yield self.list_dbs(plug)
+        self.assertFalse(dbname in dbs)
+
+        result = yield plug.call_create_database({"name": dbname})
+        user = result["user"]
+        password = result["password"]
+        self.assertEqual(result, {
+            "name": dbname,
+            "user": user,
+            "password": password,
+            "hostname": "localhost",
+            "Err": None,
+        })
+
+        dbs = yield self.list_dbs(plug)
+        self.assertTrue(dbname in dbs)
+
+    @pytest.mark.xfail(reason="The response fields are different.")
+    @inlineCallbacks
+    def test_call_create_database_idempotent(self):
+        """
+        Creating a database is idempotent and returns the same response every
+        time.
+
+        FIXME: The response contains different field names depending on whether
+        the database already exists or not. This needs to be fixed in the code.
+        """
+        dbname = "xylem_test_create_idem"
+        plug = yield self.get_plugin()
+        yield self.dropdb(plug, dbname)
+        dbs = yield self.list_dbs(plug)
+        self.assertFalse(dbname in dbs)
+
+        # Create the database
+        result = yield plug.call_create_database({"name": dbname})
+        user = result["user"]
+        password = result["password"]
+        expected_result = {
+            "name": dbname,
+            "user": user,
+            "password": password,
+            "hostname": "localhost",
+            "Err": None,
+        }
+        self.assertEqual(result, expected_result)
+        dbs = yield self.list_dbs(plug)
+        self.assertTrue(dbname in dbs)
+
+        # Recreate the database
+        result = yield plug.call_create_database({"name": dbname})
+        self.assertEqual(result, expected_result)
+        dbs = yield self.list_dbs(plug)
+        self.assertTrue(dbname in dbs)
+
+    @inlineCallbacks
+    def test_call_create_database_idempotent_broken(self):
+        """
+        Creating a database is idempotent and returns the same response every
+        time.
+
+        FIXME: The response contains different field names depending on whether
+        the database already exists or not. This needs to be fixed in the code.
+        """
+        dbname = "xylem_test_create_idem"
+        plug = yield self.get_plugin()
+        yield self.dropdb(plug, dbname)
+        dbs = yield self.list_dbs(plug)
+        self.assertFalse(dbname in dbs)
+
+        # Create the database
+        result = yield plug.call_create_database({"name": dbname})
+        user = result["user"]
+        password = result["password"]
+        expected_result = {
+            "name": dbname,
+            "user": user,
+            "password": password,
+            "hostname": "localhost",
+            "Err": None,
+        }
+        self.assertEqual(result, expected_result)
+        dbs = yield self.list_dbs(plug)
+        self.assertTrue(dbname in dbs)
+
+        # Recreate the database
+        result = yield plug.call_create_database({"name": dbname})
+        expected_result = {
+            "name": dbname,
+            "username": user,
+            "password": password,
+            "host": "localhost",
+            "Err": None,
+        }
+        self.assertEqual(result, expected_result)
+        dbs = yield self.list_dbs(plug)
+        self.assertTrue(dbname in dbs)
